0056672e0a17163104d3dd6ed7d5ecb25f811b2f
Brandon_Estrada <65381980+BrEstrada@users.noreply.github.com>
2022-02-13 22:07:10 -0700

    overhaul to be more like a dashboard

--- src/js/templates/budCard.js
+++ src/js/templates/budCard.js
@@ -3,7 +3,7 @@ import { update } from 'firebase/database';
 function budCard({ key, name, urlPath, price, type }) {
 	const template = `
     <div class="col-md-4 d-flex align-self-stretch">
-        <div class="card mb-4 box-shadow" style="width: 18rem;">
+        <div class="card mb-4 shadow" style="width: 18rem;">
 			<img class="card-img-top" src="${urlPath}" alt="${name}">
 			<div class="card-body d-flex flex-column">
                 <div class="text-center mt-auto">
@@ -12,8 +12,8 @@ function budCard({ key, name, urlPath, price, type }) {
 						price / 100
 					}</h6>
                     <p class="card-text">${type}</p>
-					<a href="#" class="btn btn-primary" id="edit">Edit</a>
-					<a href="#" class="btn btn-danger" id="delete">Delete</a>
+					<button class="btn btn-primary" id="edit" data-key="${key}">Edit</button>
+					<button class="btn btn-danger" id="delete" data-key="${key}">Delete</button>
                 </div>
 			</div>
 		</div>
@@ -29,12 +29,19 @@ function budCard({ key, name, urlPath, price, type }) {
 
 function addCardControls(product) {
 	product.querySelector('#edit').addEventListener('click', onEditProduct);
+	product.querySelector('#delete').addEventListener('click', onDeleteProduct);
 }
 
 function onEditProduct(e) {
 	const key = e.target.dataset.key;
 	sessionStorage.setItem('key', key);
 	window.location.assign('update.html');
+}
+
+function onDeleteProduct(e) {
+	const key = e.target.dataset.key;
+	sessionStorage.setItem('key', key);
+	window.location.assign('delete.html');
 }
 
 export { budCard };
